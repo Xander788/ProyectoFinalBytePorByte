268944d5ec11b079bba5c7c6f16dbe7ba9428420
Gloriana Carrillo Alfaro <glorianacarrilloalfaro@gmail.com>
2025-08-21 00:07:07 -0600

    Clase EnumTipo
    Se crea la clase EnumTipo String, constante tipo string, y sus metodos correspondientes getter, constructor y toString

---
+++ src/Vehiculos/EnumTipo.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vehiculos;
+
+/**
+ *
+ * @author Gloriana
+ */
+public enum EnumTipo {
+    PickUp("PickUp"),
+    Sedan("Sedan"),
+    Deportivos("Deportivos"),
+    Crossover("Crossover"),
+    Off_Roader("Off-Roader");
+    
+    private final String marca;
+
+    private EnumTipo(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    @Override
+    public String toString() {
+        return "Tipo: marca = " + marca + '}';
+    }
+
+   
+}
