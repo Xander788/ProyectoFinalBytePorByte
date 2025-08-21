e0f7607da4c4d3480ecedccc8c87c83710327bd3
Brayan <brazofeifaam@est.utn.ac.cr>
2025-08-20 22:56:35 -0600

    Clase List GUI
    Se creo una clase de tipo interface para los métodos de Interfaz Grafica de Usuario, donde se implementaron métodos como guardar, eliminar, limpiar, buscar, actualizar entre otros .

---
+++ src/List/ListGui.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package List;
+
+/**
+ *
+ * @author Braya
+ */
+public interface ListGui {
+    
+    public void save();
+    
+    public void delete();
+    
+    public void update();
+    
+    public void search();
+    
+    public void clear();
+    
+    public void showdata();
+}
