4a88c5cf68e0765138eb92ebd2ba3f527504b4ae
Gloriana Carrillo Alfaro <glorianacarrilloalfaro@gmail.com>
2025-08-20 23:43:29 -0600

    Paquete PersonaCliente y clase Cliente
    Se crea la clase cliente en su paquete PersonaCliente con su atributo licencia y su metodo regex de validacion, ademas de metodos getter, setter, constructor y toString

---
+++ src/PersonaCliente/Cliente.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PersonaCliente;
+import Personas.Persona;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Gloriana
+ */
+public class Cliente extends Persona {
+   private String licencia;
+   
+   //metodos
+    public String getLicencia() {
+        return licencia;
+    }
+
+    public void setLicencia(String licencia) {
+        if(validarLicencia(licencia))
+            this.licencia = licencia;
+    }
+
+    public static boolean validarLicencia(String licencia){
+        return licencia.matches("^[A-G][0-9]{6}$");
+    }
+    
+    //constructor
+
+    public Cliente(String licencia, String cedula, String nombre, LocalDate birthDate, String telefono, String correo) {
+        super(cedula, nombre, birthDate, telefono, correo);
+        if(validarLicencia(licencia)){
+        this.licencia = licencia;}
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente: Cedula: " + cedula + ", Nombre:" + nombre + ", Edad" + ", Telefono:" + telefono + ", Correo:" + correo + "Licencia: " + licencia + '}';
+    }
+}
