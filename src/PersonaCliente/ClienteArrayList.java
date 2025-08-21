1895b133fb4af74b0fae2063bcf480b9fbd03df0
Gloriana Carrillo Alfaro <glorianacarrilloalfaro@gmail.com>
2025-08-21 01:26:27 -0600

    Clase ClienteArrayList
    Se crea la clase ClienteArrayList que implementa metodos de list, se sobreescriben los metodos abstractos ademas se crea un ArrayList para almacenar Cliente

---
+++ src/PersonaCliente/ClienteArrayList.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PersonaCliente;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import List.List;
+
+
+/**
+ *
+ * @author Gloriana
+ */
+public class ClienteArrayList implements List<Cliente>{
+     ArrayList<Cliente> Array;
+
+    public ClienteArrayList() {
+        this.Array = new ArrayList();
+    }
+
+    @Override
+    public boolean add(Cliente t) {
+        if (t != null) {
+            return Array.add(t);
+        }
+        return false;    }
+
+    @Override
+    public boolean delete(Cliente t) {
+        return Array.remove(t);    
+    }
+    
+    @Override
+    public void showall() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Cliente find(Object cedula) {
+        for (Cliente t : Array) {
+            if (t.getCedula().equals(cedula)) {
+                return t;
+            }
+        }
+        return null;    
+    }
+    public Cliente get(int index) {
+        return Array.get(index);
+    }
+}
