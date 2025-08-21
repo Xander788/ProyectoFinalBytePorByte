0acf6326b4e17b2aa10ff4c96cb6d27274d2c6f2
Gloriana Carrillo Alfaro <glorianacarrilloalfaro@gmail.com>
2025-08-21 01:28:58 -0600

    Clase EmpleadoArrayList
    Se crea la clase EmpleadoArrayList que implementa metodos de list, se sobreescriben los metodos abstractos ademas se crea un ArrayList para almacenar Empleados

---
+++ src/PersonaEmpleado/EmpleadoArrayList.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PersonaEmpleado;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import List.List;
+
+
+/**
+ *
+ * @author Gloriana
+ */
+public class EmpleadoArrayList implements List<Empleado>{
+     ArrayList<Empleado> Array;
+
+    public EmpleadoArrayList() {
+        this.Array = new ArrayList();
+    }
+
+    @Override
+    public boolean add(Empleado t) {
+        if (t != null) {
+            return Array.add(t);
+        }
+        return false;    }
+
+    @Override
+    public boolean delete(Empleado t) {
+        return Array.remove(t);    
+    }
+    
+    @Override
+    public void showall() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Empleado find(Object cedula) {
+        for (Empleado t : Array) {
+            if (t.getCedula().equals(cedula)) {
+                return t;
+            }
+        }
+        return null;    
+    }
+    public Empleado get(int index) {
+        return Array.get(index);
+    }
+}
