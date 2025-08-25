40c378c4e7750d86f472c8ceb4da670b76db7743
Xander78_ <alramirezba@est.utn.ac.cr>
2025-08-24 21:50:26 -0600

    ReservasQueue
    Se implementa de List los metodos y se programan

---
+++ src/Reservas/ReservasQueue.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Reservas;
+
+import List.List;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ * @author alexr
+ */
+public class ReservasQueue implements List<Reserva>{
+private Queue<Reserva> queue = new LinkedList<>();
+
+    @Override
+    public boolean add(Reserva t) {
+        return queue.offer(t);
+    }
+
+    @Override
+    public boolean delete(Reserva t) {
+        return queue.remove(t);
+    }
+
+    @Override
+    public Reserva find(Object id) {
+        for (Reserva r : queue) {
+            if (r.getCedulaCliente().equals(id)) {
+                return r;
+            }
+        }
+        return null;
+
+    }
+
+    @Override
+    public void showall() {
+        for (Reserva r : queue) {
+            System.out.println(r);
+        }
+    }
+
+    public Queue<Reserva> getQueue() {
+        return queue;
+    }
+    
+}
